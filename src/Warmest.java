9e3c38a4dff82ab3fef7dc4d9ab59ea523f327ac
Romyshufman <romyshufman@mail.tau.ac.il>
2022-08-23 14:43:26 +0300

    done- remove option 2

--- src/Warmest.java
+++ src/Warmest.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class Warmest<K,V> {
 
     private Map<K,V> elements= new HashMap<K,V>();
-    private LinkedList<V> warm_list = new LinkedList<V>();
+    private Map<V,K> elements_reverse= new HashMap<V,K>();
+    //private LinkedList<V> warm_list = new LinkedList<V>();
+    private Stack <V> warm_stack = new Stack<V>();
 
     public void put(K key, V value){
         elements.put(key,value);
-        warm_list.add(value);
+        elements_reverse.put(value,key);
+        //warm_list.add(value);
+        warm_stack.push(value);
     }
 
     public V get(K k){
@@ -15,7 +19,8 @@ public class Warmest<K,V> {
             return null;
         }
         V val= elements.get(k);
-        warm_list.add(val);
+        //warm_list.add(val);
+        warm_stack.push(val);
         return val;
     }
 
@@ -25,12 +30,16 @@ public class Warmest<K,V> {
         }
         V val = elements.get(k);
         elements.remove(k);
-        ListIterator<V> iter = (ListIterator<V>) warm_list.iterator();
-        while (iter.hasNext()) {
-            V node = iter.next();
-            if (node.equals(val)) {
-                iter.remove();
-            }
+        elements_reverse.remove(val);
+//        ListIterator<V> iter = (ListIterator<V>) warm_list.iterator();
+//        while (iter.hasNext()) {
+//            V node = iter.next();
+//            if (node.equals(val)) {
+//                iter.remove();
+//            }
+//        }
+        while(!warm_stack.isEmpty() && !elements_reverse.containsKey(warm_stack.peek())){
+            warm_stack.pop();
         }
         return val;
     }
@@ -39,13 +48,18 @@ public class Warmest<K,V> {
         if (elements.isEmpty()){
             return null;
         }
-        return warm_list.getLast();
+//        return warm_list.getLast();
+        return warm_stack.peek();
     }
 
-    private void printWarmList(){
-        for (int i = 0; i < warm_list.size(); i++) {
-            System.out.println(i +" = "+warm_list.get(i));
-        }
+//    private void printWarmList(){
+//        for (int i = 0; i < warm_list.size(); i++) {
+//            System.out.println(i +" = "+warm_list.get(i));
+//        }
+//    }
+    public void printWarmStack(){
+        System.out.println(warm_stack.toString());
     }
 }
+
 
